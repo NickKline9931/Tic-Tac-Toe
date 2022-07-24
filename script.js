43c9a1a2a3893ce337e1f646a92673dccd003fe4
Nick <nickkline9931@gmail.com>
2022-07-24 14:41:14 -0400

    Add win alerts

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const fillSquare = (() => {
-const squares = document.querySelectorAll('.square');
+    const squares = document.querySelectorAll('.square');
 let turn = 1;
 squares.forEach(square => {
     square.addEventListener('click', function () {
@@ -10,6 +10,38 @@ squares.forEach(square => {
             square.innerHTML = "X";
             turn++;
         }
+        checkWin();
     }, {once: true});
 });
+function checkWin() {
+    const s1 = document.getElementById("one");
+    const s2 = document.getElementById("two");
+    const s3 = document.getElementById("three");
+    const s4 = document.getElementById("four");
+    const s5 = document.getElementById("five");
+    const s6 = document.getElementById("six");
+    const s7 = document.getElementById("seven");
+    const s8 = document.getElementById("eight");
+    const s9 = document.getElementById("nine");
+    
+    if((s1.innerHTML == "X") && (s2.innerHTML == "X") && (s3.innerHTML == "X") || 
+    (s4.innerHTML == "X") && (s5.innerHTML == "X") && (s6.innerHTML == "X") ||
+    (s7.innerHTML == "X") && (s8.innerHTML == "X") && (s9.innerHTML == "X") ||
+    (s1.innerHTML == "X") && (s4.innerHTML == "X") && (s7.innerHTML == "X") ||
+    (s2.innerHTML == "X") && (s5.innerHTML == "X") && (s8.innerHTML == "X") ||
+    (s3.innerHTML == "X") && (s6.innerHTML == "X") && (s9.innerHTML == "X") ||
+    (s1.innerHTML == "X") && (s5.innerHTML == "X") && (s9.innerHTML == "X") ||
+    (s3.innerHTML == "X") && (s5.innerHTML == "X") && (s7.innerHTML == "X"))  {
+        alert("X wins!");
+    } else if((s1.innerHTML == "O") && (s2.innerHTML == "O") && (s3.innerHTML == "O") ||
+    (s4.innerHTML == "O") && (s5.innerHTML == "O") && (s6.innerHTML == "O") ||
+    (s7.innerHTML == "O") && (s8.innerHTML == "O") && (s9.innerHTML == "O") ||
+    (s1.innerHTML == "O") && (s4.innerHTML == "O") && (s7.innerHTML == "O") ||
+    (s2.innerHTML == "O") && (s5.innerHTML == "O") && (s8.innerHTML == "O") ||
+    (s3.innerHTML == "O") && (s6.innerHTML == "O") && (s9.innerHTML == "O") ||
+    (s1.innerHTML == "O") && (s5.innerHTML == "O") && (s9.innerHTML == "O") ||
+    (s3.innerHTML == "O") && (s5.innerHTML == "O") && (s7.innerHTML == "O")) {
+        alert("O wins!")
+    }
+}
 })();
